7d013e02403bd7ee5402912ae67eab6debf056e3
Pontus Stjernström <Pontus Stjernstrm>
2016-11-30 20:08:44 +0100

    Just warming up with some windows specific libraries n' stuff.

--- RabbitsSlaughter/Main.cpp
+++ RabbitsSlaughter/Main.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
+#define WINDOWS
+
+#ifdef WINDOWS
+#include <Windows.h>
+#endif
+
 using namespace std;
 
 int main(int argc, char* args[])
 {
+
+	printf("Hello Alex!\n");
+
+#ifdef WINDOWS
+	Sleep(3000);
+#else
+	scanf("%c");
+#endif
 
 	return 0;
 }
