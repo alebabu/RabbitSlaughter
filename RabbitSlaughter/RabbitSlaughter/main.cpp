22681c21c576fb7e7c2d3746017899b77ed1c25a
Aleksandar Babunovic <aleksandarbabunovic@gmail.com>
2016-12-02 16:20:24 +0100

    I put the files correct for my editor but i don't know if it will work on yours

---
+++ RabbitSlaughter/RabbitSlaughter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RabbitSlaughter
+//
+//  Created by Aleksandar Babunovic on 2016-12-02.
+//  Copyright © 2016 Aleksandar Babunovic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
