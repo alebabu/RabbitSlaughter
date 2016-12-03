7a30372c939222fffa712b3f664453e89cdcdf43
Aleksandar Babunovic <aleksandarbabunovic@gmail.com>
2016-12-03 16:14:27 +0100

    Now there is only one src file

---
+++ src/model/Player.h
@@ -0,0 +1,30 @@
+//
+//  Player.h
+//  RabbitSlaughter
+//
+//  Created by Aleksandar Babunovic on 2016-12-01.
+//  Copyright © 2016 Aleksandar Babunovic. All rights reserved.
+//
+
+#ifndef Player_h
+#define Player_h
+
+class Player {
+    public:
+    Player(int);
+    Player();
+    ~Player();
+    // get and set functions
+    int getX();
+    int getY();
+    void setX(int);
+    void setY(int);
+    void move();
+    
+
+    private:
+    int x;
+    int y;
+};
+
+#endif  // Player_h 
